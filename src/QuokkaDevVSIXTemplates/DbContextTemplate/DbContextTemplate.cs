23abc85e7f037ad645efb350904583d9a0a8a182
Fabio Cometti <fabio.cometti@gmail.com>
2022-06-15 09:34:16 +0200

    fix(templates): fix templates
    fix new templates

--- src/QuokkaDevVSIXTemplates/DbContextTemplate/DbContextTemplate.cs
+++ src/QuokkaDevVSIXTemplates/DbContextTemplate/DbContextTemplate.cs
@@ -1,76 +1,79 @@
-﻿namespace $rootnamespace$
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace $rootnamespace$
 {
     internal class $safeitemname$ : DbContext, IUnitOfWork
     {
         private readonly IDomainEventsDispatcher eventsDispatcher;
 
-/// <summary>
-/// Default constructor
-/// </summary>
-public $safeitemname$()
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public $safeitemname$()
         {
             eventsDispatcher = null!;
         }
 
-/// <summary>
-/// Constructor
-/// </summary>
-/// <param name="context">Configuration options</param>
-public $safeitemname$(DbContextOptions <$safeitemname$> context, IDomainEventsDispatcher eventsDispatcher) : base(context)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context">Configuration options</param>
+        public $safeitemname$(DbContextOptions <$safeitemname$> context, IDomainEventsDispatcher eventsDispatcher) : base(context)
         {
-    this.eventsDispatcher = eventsDispatcher;
-}
+            this.eventsDispatcher = eventsDispatcher;
+        }
 
-#region Db Sets
+        #region Db Sets
 
-//public DbSet<MyAggregateRoot> MyAggregateRoots => Set<MyAggregateRoot>();
+        //public DbSet<MyAggregateRoot> MyAggregateRoots => Set<MyAggregateRoot>();
 
-#endregion
+        #endregion
 
-/// <summary>
-/// Configure EF model
-/// </summary>
-/// <param name="modelBuilder"></param>
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-    //modelBuilder.Entity<MyAggregateRoot>(ConfigureMyAggregateRoot);
-    //modelBuilder.ApplyConfigurationsFromAssembly(typeof($safeitemname$).Assembly);
-}
-
-/// <summary>
-/// Save entitities
-/// </summary>
-/// <param name="cancellationToken"></param>
-/// <returns></returns>
-/// <exception cref="NotImplementedException"></exception>
-public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
-{
-    await DispatchDomainEventsAsync(cancellationToken);
-    var result = await base.SaveChangesAsync();
-
-    return true;
-}
-
-private async Task DispatchDomainEventsAsync(CancellationToken cancellationToken = default)
-{
-    var domainEntities = this.ChangeTracker
-        .Entries<Entity>()
-        .Where(x => x.Entity.DomainEvents?.Any() == true);
-
-    var domainEvents = domainEntities
-        .SelectMany(x => x.Entity.DomainEvents!)
-        .ToList();
-
-    domainEntities.ToList()
-        .ForEach(entity => entity.Entity.DomainEvents!.Clear());
-
-    var tasks = domainEvents
-        .Select(async (domainEvent) =>
+        /// <summary>
+        /// Configure EF model
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            await eventsDispatcher.Publish(domainEvent, cancellationToken);
-        });
+            //modelBuilder.Entity<MyAggregateRoot>(ConfigureMyAggregateRoot);
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof($safeitemname$).Assembly);
+        }
 
-    await Task.WhenAll(tasks);
-}
+        /// <summary>
+        /// Save entitities
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            await DispatchDomainEventsAsync(cancellationToken);
+            var result = await base.SaveChangesAsync();
+
+            return true;
+        }
+
+        private async Task DispatchDomainEventsAsync(CancellationToken cancellationToken = default)
+        {
+            var domainEntities = this.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.DomainEvents?.Any() == true);
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.DomainEvents!)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.DomainEvents!.Clear());
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) =>
+                {
+                    await eventsDispatcher.Publish(domainEvent, cancellationToken);
+                });
+
+            await Task.WhenAll(tasks);
+        }
     }
 }
--- src/QuokkaDevVSIXTemplates/EntityConfigurationTemplate/EntityConfigurationTemplate.cs
+++ src/QuokkaDevVSIXTemplates/EntityConfigurationTemplate/EntityConfigurationTemplate.cs
@@ -1,10 +1,13 @@
-﻿namespace StorePortal.Infrastructure.SchemaDefinitions
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace $rootnamespace$
 {
-    public class $safeitemname$Configuration : IEntityTypeConfiguration<$safeitemname$>
+    public class $safeitemname$ : IEntityTypeConfiguration<object>
     {
-        public void Configure(EntityTypeBuilder<$safeitemname$> builder)
-    {
-        builder.ToTable("MyEntityTableName");
+        public void Configure(EntityTypeBuilder<object> builder)
+        {
+            builder.ToTable("MyEntityTableName");
+        }
     }
-}
 }
